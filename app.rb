d9da4aad19c33636054f848f399b0a497dea5669
Michael Cetrulo <mail2samus@gmail.com>
2014-08-14 04:47:14 -0300

    parse webservice stub

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'json'
 
 get '/' do
   erb :index
+end
+
+post '/parse' do
+  content_type 'application/json'
+
+  response = {}
+  if params.has_key? 'target' and params.has_key? 'template'
+    response[:status] = "ok"
+    response[:result] = {:foo => 'lala'}
+  else
+    status 400
+    response[:status]  = "error"
+    response[:message] = "Required parameters: target, template"
+  end
+
+  response.to_json
 end
